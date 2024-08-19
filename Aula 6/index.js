ea9e1e437933f50d4f20582939d8d2f60b4247e8
LiviaSobral <134714074+LiviaSobral@users.noreply.github.com>
2024-08-19 17:05:39 -0300

    Add files via upload

---
+++ Aula 6/index.js
@@ -0,0 +1,80 @@
+/*const primeiroValor = 10
+const segundoValor = 20
+
+const resultado = primeiroValor + segundoValor +4
+console.log("O resultado da conta é:",resultado)*/
+
+/*const primeiroValor = 10
+const segundoValor = 20
+
+const resultado = primeiroValor - segundoValor
+console.log("O resultado da conta é:",resultado)*/
+
+/*const primeiroValor = 10
+const segundoValor = 25
+
+const resultado = primeiroValor * segundoValor
+
+console.log("O resultado da conta é:",resultado)*/
+
+/*const primeiroValor = 345
+const segundoValor = 10
+
+const resultado = primeiroValor/segundoValor
+
+console.log("O resultado da conta é:",resultado)*/
+/*const restoDivisão = 11%4
+console.log("O resto da divisão é:",restoDivisão)*/
+
+/*let numero1 = 3
+let numero2 = 4
+let resultado
+resultado = numero1 + numero2
+console.log("Primeiro resultado é: ",resultado)
+numero1 = 3
+numero2 = 5
+resultado = (numero1 * numero2)/2
+console.log("Segundo resultado é: ",resultado)
+numero1 = 5
+numero2 = 4
+resultado = (numero2 - numero1)*-1
+console.log("Terceiro resultado é: ",resultado)
+resultado = 234%5
+console.log("Quarto resultado é: ",resultado)*/
+
+/*let numero1 = prompt("escreva um numero")
+let numero2 = prompt("escreva um numero")
+let comparador = numero1 === numero2
+console.log("Os numeros", numero1,"e", numero2,"===",comparador)*/
+
+/*let numero1 = prompt("escreva um numero")
+let numero2 = prompt("escreva um numero")
+let comparador = numero1 !== numero2
+console.log("Os numeros", numero1,"e", numero2,"!==",comparador)*/
+
+/*let numero1 = prompt("escreva um numero")
+let numero2 = prompt("escreva um numero")
+let comparador = numero1 >= numero2
+console.log("Os numeros", numero1,">=", numero2,"=",comparador)*/
+
+/*let numero1 = prompt("escreva um numero")
+let numero2 = prompt("escreva um numero")
+let comparador = numero1 <= numero2
+console.log("Os numeros", numero1,"<=", numero2,"=",comparador)
+comparador = numero1 < numero2
+console.log("Os numeros", numero1,"<", numero2,"=",comparador)*/
+//exercicios
+let primeiroNumero = 1
+let segundoNumero = 11
+let comparador = primeiroNumero === segundoNumero
+console.log("Os numeros", primeiroNumero,"===", segundoNumero,"=",comparador)
+comparador = primeiroNumero !== segundoNumero
+console.log("Os numeros", primeiroNumero,"!==", segundoNumero,"=",comparador)
+comparador = primeiroNumero > segundoNumero
+console.log("Os numeros", primeiroNumero,">", segundoNumero,"=",comparador)
+comparador = primeiroNumero < segundoNumero
+console.log("Os numeros", primeiroNumero,"<", segundoNumero,"=",comparador)
+
+
+
+
