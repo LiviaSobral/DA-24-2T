db5c305fee9425a631359c1db5b1a7a141909888
LiviaSobral <Liviaheloisa2005@gmail.com>
2024-09-30 17:16:35 -0300

    atividades

---
+++ Atividade Avaliativa C/index.js
@@ -0,0 +1,186 @@
+personagens =[{nome:"Link",vida:20,forca:10,resistencia:7}]
+armas =[{nome:"Espada",dano:6,alcance:0},{nome:"Espada Longa",dano:7,alcance:1},{nome:"Arco",dano:5,alcance:10}]
+itens =[{nome:"Poção de vida",efeito:"Regeneração de vida"},{nome:"Rupees",efeito:"Nenhum"},{nome:"Bomba",efeito:"Dano Explosivo"}]
+
+function erro(motivo,local){
+    //function para fazer retornos de erro mais rapidamente
+    frase = "ERRO: "+local+" tem "+motivo+" Invalido"
+    alert(frase)
+    menuPrincipal()
+}
+
+function lista(){
+    //função para listar os personagens, armas e itens existentes
+    frase ="Os Personagens São    ||  "
+    for(personagen of personagens){
+        frase += "Nome: "+personagen.nome+" Vida: "+personagen.vida+" Forca: "+personagen.forca+" Resistencia: "+personagen.resistencia+"  ||    "
+        //roda por todos os personagens do array de personagens e salva as informações deles na variavel frase
+    }
+    alert(frase)
+    frase = "As Armas São   ||  "
+    //substitui o conteudo anterior da variavel
+    for(arma of armas){
+        frase += "Nome: "+arma.nome+" Dano: "+arma.dano+" Alcance: "+arma.alcance+"  ||    "
+        //roda por todos as armas do array de armas e salva as informações delas na variavel frase
+    }
+    alert(frase)
+    frase = "Os Itens São   ||  "
+    //substitui o conteudo anterior da variavel
+    for(iten of itens){
+        frase += "Nome: "+iten.nome+" Efeito: "+iten.efeito+"  ||    "
+        //roda por todos os itens do array de itens e salva as informações deles na variavel frase
+    }
+    alert(frase)
+    menuPrincipal()
+}
+
+function menuPrincipal(){
+    //O menu principal onde as funções podem ser acessadas e iniciadas
+    resposta=Number(prompt("Escolha uma das opções (1)Lista (2)Validar Personagen (3)Validar Arma (4) Validar Iten"))
+
+    if(resposta===4){
+        validarItens()
+        //chama função validar itens
+    }else if(resposta===2){
+        validarPersonagens()
+        //chama função validar personagens
+    }else if(resposta===3){
+        validarArmas()
+        //chama função validar armas
+    }else{
+        lista()
+        //chama função lista
+    }
+
+
+}
+
+
+
+
+
+function validarItens(){
+    //função para validar itens
+    a=false
+    b=false
+    //duas variaveis com false para eu poder salvar se algo existe no array igual com o que eu procurar
+    nome1 = prompt("Digite o nome")
+    for(i=0;i<itens.length;i++){
+        if(nome1===itens[i].nome){
+            a=true
+        }}
+        if(a===false){
+            return erro("Nome","Iten")
+        }
+        //comparação do nome
+    efeito1 = prompt("Digite o Efeito")
+    for(i=0;i<itens.length;i++){
+        if(efeito1===itens[i].efeito&&nome1===itens[i].nome){
+            b=true
+        }}
+        if(b===false){
+            return erro("Efeito","Iten")
+        }
+        //comparação do efeito
+        alert("Iten Validado com sucesso")
+        //aviso de sucesso
+    
+    menuPrincipal()
+    //retorna para o menu principal
+}
+
+function validarPersonagens(){
+    //função para validar personagens
+    a=false
+    b=false
+    c=false
+    d=false
+    //variaveis com false para eu poder salvar se algo existe no array igual com o que eu procurar
+    nome1 = prompt("Digite o nome")
+    for(per of personagens){
+        if(nome1===per.nome){
+            a=true
+        }}
+        if(a===false){
+       
+            return erro("Nome","Personagen")
+        }
+        //comparação do nome
+    vida1 = Number(prompt("Digite o Valor de Vida"))
+        if(vida1>=1&&vida1<=20){
+            d=true
+        }
+        if(d===false){ return erro("Vida","Personagen") }
+        //comparação da vida não pode ser nem menor que 1 ou maior que 20
+    forca1 = Number(prompt("Digite o Valor de Força"))
+    for(per of personagens){
+        if(forca1===per.forca&&nome1===per.nome){
+            b=true
+        }}
+        if(b===false){
+            return erro("Valor de Força","Personagen")
+        }
+        //comparação da força
+    resistencia1 = Number(prompt("Digite o Valor de Resistencia"))
+    for(per of personagens){
+        if(resistencia1===per.resistencia&&nome1===per.nome){
+            c=true
+        }}
+        if(c===false){
+            return erro("Valor de Resistencia","Personagen")
+        }
+        //comparação da resistencia
+
+        
+
+    
+    
+    alert("Persongen Validado com sucesso")
+    //aviso de sucesso
+
+    menuPrincipal()
+    //retorna ao menu principal
+}
+
+function validarArmas(){
+    //função para validar armas
+    a=false
+    b=false
+    c=false
+    //variaveis com false para eu poder salvar se algo existe no array igual com o que eu procurar
+    nome1 = prompt("Digite o nome")
+    for(i=0;i<armas.length;i++){
+        if(nome1===armas[i].nome){
+            a=true
+        }}
+        if(a===false){
+            return erro("Nome","Arma")
+        }
+        //comparação do nome
+    dano1 = Number(prompt("Digite o Valor de Dano"))
+    for(i=0;i<armas.length;i++){
+        if(dano1===armas[i].dano&&nome1===armas[i].nome){
+            b=true
+        }}
+        if(b===false){
+            return erro("Valor de Dano","Arma")
+        }
+        //comparação do dano
+    alcance1 = Number(prompt("Digite O Valor de Alcance"))
+    for(i=0;i<armas.length;i++){
+        if(alcance1 === armas[i].alcance&&nome1===armas[i].nome){
+            c=true
+        }}
+        if(c===false){
+            return erro("Valor de Alcance","Arma")
+        }
+        //comparação do alcance
+  
+alert("Arma Validada com sucesso")
+//aviso de sucesso
+    
+    menuPrincipal()
+    //retorna ao menu principal
+}
+
+menuPrincipal()
---
+++ Aula21/index.js
@@ -0,0 +1,96 @@
+/*let matriz=[
+    [2,4,6],
+    [8,10,12],
+    [14,16,18]
+]
+
+console.log(matriz[2][1])
+//vai imprimir 16 pois é o numero localizado na linha 2 e coluna 1*/
+
+/*let matriz = [
+    [5,10,15],
+    [20,25,30],
+    [35,40,45]
+]
+
+let soma = 0
+for(let i = 0;i<matriz.length;i++){
+    for(let j=0;j<matriz[i].length;j++){
+        soma +=matriz[i][j]
+    }
+}
+
+console.log(soma)
+//o numero que sera imprimido é 225 isso ocorre pois ele soma cada numero da matriz e guarda na variavel soma*/
+
+/*mapa=[
+    [1,1,1,1,1],
+    [1,2,0,0,1],
+    [1,0,2,0,1],
+    [1,0,0,0,1],
+    [1,1,1,1,1]
+]
+frase = ""
+for(i=0;i< mapa.length;i++){
+    for(j=0;j<mapa[i].length;j++){
+        frase+= mapa[i][j]
+        if(j===4){
+            frase+="                                                  "
+        }
+    }
+}
+console.log(frase)*/
+
+/*let pontos =[
+    [2,5,3,6],
+    [1,7,9,3],
+    [6,2,7,3]
+]
+let total1=0
+let total2=0
+let total3=0
+
+
+for(i=0;i<pontos.length;i++){
+    for(j=0;j<pontos[i].length;j++){
+        if(i===0){
+            total1 +=pontos[i][j]
+        }
+        else if(i===1){
+            total2+=pontos[i][j]
+        }
+        else if(i===2){
+            total3+=pontos[i][j]
+        }
+    }
+}
+
+console.log("Jogador 1 fez:",total1,"Pontos")
+console.log("Jogador 2 fez:",total2,"Pontos")
+console.log("Jogador 3 fez:",total3,"Pontos")*/
+
+/*function criaMatriz(){
+    return matriz = [
+        [1,2,3,4],
+        [5,6,7,8],
+        [9,10,11,12],
+        [13,14,15,16]
+    ]
+}
+console.log(criaMatriz())
+function inverteMatriz(matriz){
+    let c=0
+    return matriz=[
+        [matriz[0][3],matriz[0][2],matriz[0][1],matriz[0][0]],
+        [matriz[1][3],matriz[1][2],matriz[1][1],matriz[1][0]],
+        [matriz[2][3],matriz[2][2],matriz[2][1],matriz[2][0]],
+        [matriz[3][3],matriz[3][2],matriz[3][1],matriz[3][0]],
+    ]
+}
+console.log(inverteMatriz(criaMatriz()))*/
+
+
+
+
+
+
